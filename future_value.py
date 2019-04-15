4e77ff5ce6123c8bc0aad2ee7956b404e2dc25ac
Z1782108 <47008182+Z1782108@users.noreply.github.com>
2019-04-14 21:02:18 -0500

    Add files via upload

--- future_value.py
+++ future_value.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
         
-import validation as v
-               
+def get_float(prompt, low, high):
+    while True:
+        number = float(input(prompt))
+        if number > low and number <= high:
+            return number
+        else:
+            print("Entry must be greater than", low,
+                  "and less than or equal to", high,
+                  "Please try again.")
+            
+def get_int(prompt, low, high):
+    while True:
+        number = int(input(prompt))
+        if number > low and number <= high:
+            return number
+        else:
+            print("Entry must be greater than", low,
+                  "and less than or equal to", high,
+                  "Please try again.")
+            
 def calculate_future_value(monthly_investment, yearly_interest, years):
+    print("Entering calculate_future_value()")
     monthly_interest_rate = yearly_interest / 12 / 100
     months = years * 12
 
-    #FV Calculate
     future_value = 0.0
-    for i in range(months):
+    for i in range(1, months+1):           
         future_value += monthly_investment
         monthly_interest = future_value * monthly_interest_rate
         future_value += monthly_interest
-
+        print("i =", i, "future value =", future_value)
     return future_value
 
 def main():
     choice = "y"
     while choice.lower() == "y":
-        # asking for inputs
-        monthly_investment = v.get_float("Enter monthly investment:\t", 0, 1000)
-        yearly_interest_rate = v.get_float("Enter yearly interest rate:\t", 0, 15)
-        years = v.get_int("Enter number of years:\t\t", 0, 50)
+        # get input from the user
+        monthly_investment = get_float("Enter monthly investment:\t", 0, 1000)
+        yearly_interest_rate = get_float("Enter yearly interest rate:\t", 0, 15)
+        years = get_int("Enter number of years:\t\t", 0, 50)
 
         future_value = calculate_future_value(
             monthly_investment, yearly_interest_rate, years)
@@ -30,10 +48,10 @@ def main():
         print("Future value:\t\t\t" + str(round(future_value, 2)))
         print()
 
-        choice = input("Would you like to continue? (y/n): ")
+        choice = input("Continue? (y/n): ")
         print()
 
-    print("See you later!!")
+    print("See ya!")
     
 if __name__ == "__main__":
     main()
